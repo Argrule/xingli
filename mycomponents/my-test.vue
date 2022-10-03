1f3f32c5033ab7a8e1cfbfaf319f6660da85de41
Argrule <99395773+Argrule@users.noreply.github.com>
2022-10-04 01:41:30 +0800

    test页面图片

--- mycomponents/my-test.vue
+++ mycomponents/my-test.vue
@@ -1,20 +1,37 @@
 <template>
-    <view>hello test</view>
+  <view>
+    <view class="headPart">
+      <u--image
+        :src="photoUrl"
+        width="330rpx"
+        height="450rpx"
+        radius="15"
+      ></u--image>
+      <u--image
+        :src="photoUrl"
+        width="330rpx"
+        height="450rpx"
+        radius="15"
+      ></u--image>
+    </view>
+  </view>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-
-        }
-    },
-    methods:{
-
-    }
-}
+  data() {
+    return {
+      photoUrl: "https://cdn.uviewui.com/uview/album/8.jpg",
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style lang="less" scoped>
-
+.headPart {
+  display: flex;
+  justify-content: space-evenly;
+  margin: 15rpx 0;
+}
 </style>
