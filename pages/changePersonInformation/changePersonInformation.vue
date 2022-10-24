9e04c9fde9d53c7db9a71a8b2e6967696905c3cc
Argrule <99395773+Argrule@users.noreply.github.com>
2022-10-24 21:58:40 +0800

    修改个人信息完善，只差换头像，需要图床

--- pages/changePersonInformation/changePersonInformation.vue
+++ pages/changePersonInformation/changePersonInformation.vue
@@ -12,12 +12,17 @@
       </u-cell>
       <u-cell title="昵称" isLink @click="inputDialogToggle">
         <view slot="value">
-          {{userName}}
+          {{ userName }}
         </view>
       </u-cell>
-      <u-cell title="性别" isLink="true" @click="showSexSelect = true">
+      <u-cell title="性别" isLink @click="showSexSelect = true">
         <view slot="value">
-          {{gender==1?'男':'女'}}
+          {{ gender == 1 ? "男" : "女" }}
+        </view>
+      </u-cell>
+      <u-cell title="邮箱" isLink @click="emailDialogToggle">
+        <view slot="value">
+          {{ email }}
         </view>
       </u-cell>
     </u-cell-group>
@@ -47,24 +52,47 @@
         @confirm="dialogInputConfirm"
       ></uni-popup-dialog>
     </uni-popup>
+    <!-- 填写邮箱 -->
+    <uni-popup ref="inputEmailName" type="dialog">
+      <uni-popup-dialog
+        ref="inputClose"
+        mode="input"
+        title="填写邮箱"
+        :value="theEmail"
+        @close="dialogClose"
+        @confirm="emailInputConfirm"
+      ></uni-popup-dialog>
+    </uni-popup>
   </view>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapMutations } from "vuex";
 export default {
   data() {
     return {
       // 选择性别弹窗
       showSexSelect: false,
       theGender: 1,
-      nickName: "刘荣",
+      nickName: "",
+      theEmail: "",
     };
   },
   computed: {
-    ...mapState("m_personal", ["avatarUrl","userId","userName","gender"]),
+    ...mapState("m_personal", [
+      "avatarUrl",
+      "userId",
+      "userName",
+      "gender",
+      "email",
+    ]),
   },
   methods: {
+    ...mapMutations("m_personal", [
+      "changeGender",
+      "changeUserName",
+      "changeEmail",
+    ]),
     // 选择性别后触发
     selectClick({ name }) {
       this.theGender = name == "男" ? 1 : 0;
@@ -78,15 +106,25 @@ export default {
     dialogClose() {
       console.log("dialogClose,取消就不发请求");
     },
-    // 提交
+    // 提交nickName
     dialogInputConfirm(str_value) {
       console.log("dialogInputConfirm");
       // 修改昵称
       this.putPersonInfo(str_value);
     },
+    // 提交email
+    emailInputConfirm(str_value) {
+      console.log("emailInputConfirm");
+      // 修改email
+      this.putPersonInfo(str_value, 2);
+    },
     // 打开input nickName
     inputDialogToggle() {
       this.$refs.inputNickName.open();
+    },
+    // 打开input email弹窗
+    emailDialogToggle() {
+      this.$refs.inputEmailName.open();
     },
     // 各种修改信息put
     async putPersonInfo(str_value, key = 0) {
@@ -95,16 +133,36 @@ export default {
       };
       switch (key) {
         case 0:
-          putInfo.nickName = str_value;
+          if (str_value == "") {
+            return uni.$showMsg("名字不能为空");
+          }
+          putInfo.userName = str_value;
           break;
         case 1:
           putInfo.gender = str_value;
           break;
-        default:
+        case 2:
+          if (!/[\w\W]+@[\w\W]+/.test(str_value)) {
+            return uni.$showMsg("邮箱格式错误");
+          }
+          putInfo.email = str_value;
           break;
       }
       const { data: res } = await uni.$http.post("/user/info", putInfo);
       console.log("put info", res);
+      if (res.code == "00000") {
+        switch (key) {
+          case 0:
+            this.changeUserName(str_value);
+            break;
+          case 1:
+            this.changeGender(str_value);
+            break;
+          case 2:
+            this.changeEmail(str_value);
+            break;
+        }
+      }
     },
   },
 };
